c54e095fe020953852f0d7a2852f7008b25bad82
sergey <sergeypechnikov326@gmail.com>
2021-10-15 17:24:00 +0300

    improve example with getting type

--- examples/longpoll.rs
+++ examples/longpoll.rs
@@ -1,3 +1,5 @@
+use vkapi::longpoll::EventType;
+
 #[tokio::main]
 async fn main() {
     let mut vk_api = vkapi::VK::new("5.103", "ru", "db619ee5e3e018f25fc3a1d27153198c22d8c84850be604e3a2bb2d66a8a5d13771b56198d08ea3ac358e".into());
@@ -7,6 +9,9 @@ async fn main() {
     );
 
     for event in ch {
-        println!("Got event: {:?}", event);
+        match event.0 {
+            EventType::NewMessage => println!("new message: {}", event.1),
+            EventType::Other(name) => println!("new event type {} {}", name, event.1)
+        };
     }
 }
